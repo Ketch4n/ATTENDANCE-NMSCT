e4f15d3382357ffa79285b667ecfd39b8482e8b5
Ketch4n <mangao.christian.04@gmail.com>
2025-01-28 16:59:40 +0800

    Dashboard-Modular/Refractor

--- lib/src/view/administrator/dashboard/estab/dashboard/dashboard_page.dart
+++ lib/src/view/administrator/dashboard/estab/dashboard/dashboard_page.dart
@@ -19,171 +19,168 @@ class DashBoardEstab extends StatelessWidget {
       child: Scaffold(
         body: Align(
           alignment: Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SingleChildScrollView(
-              child: Consumer<DashboardProvider>(
-                builder: (context, provider, child) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          constraints: const BoxConstraints(
-                            maxWidth: 810,
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.asset(
-                              'assets/img/nmscst_bg.jpg',
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(
-                              width: 200,
-                              height: 50,
-                              child: DropdownButtonFormField<String>(
-                                value: provider.selectedYearRange,
-                                decoration: const InputDecoration(
-                                  label: Text("School Year"),
-                                  contentPadding: EdgeInsets.zero,
-                                ),
-                                items: provider.schoolYears
-                                    .map((String yearRange) {
-                                  return DropdownMenuItem<String>(
-                                    value: yearRange,
-                                    child: Text(yearRange),
-                                  );
-                                }).toList(),
-                                onChanged: (String? newValue) {
-                                  provider.setSelectedYearRange(newValue);
-                                },
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: provider.refresh,
-                              child: const Text("Reload Data / Refresh"),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Wrap(
-                          crossAxisAlignment: WrapCrossAlignment.center,
-                          spacing: 8.0,
-                          runSpacing: 8.0,
-                          children: <Widget>[
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) => AllLateStudent(
-                                            year: provider.selectedYearRange ??
-                                                provider.defaultYear,
-                                          )),
-                                );
-                              },
-                              child: IndexCard(
-                                count: provider.late,
-                                icon: Icons.timelapse_sharp,
-                                child: 'List of Late',
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => AllOutsideRange(
-                                        ids: provider.outsideIds),
-                                  ),
-                                );
-                              },
-                              child: IndexCard(
-                                count: provider.outside.toString(),
-                                icon: Icons.location_off_sharp,
-                                child: 'Outside Range',
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) => AllAbsentStudent(
-                                            year: provider.selectedYearRange ??
-                                                provider.defaultYear,
-                                          )),
-                                );
-                              },
-                              child: IndexCard(
-                                count: provider.absent,
-                                icon: Icons.person_off_outlined,
-                                child: 'Absent',
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const AllEstablishment()),
-                                );
-                              },
-                              child: IndexCard(
-                                count: provider.count_estab,
-                                icon: Icons.location_city,
-                                child: 'All Establishment',
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => CoursesPage(
-                                          year: provider.selectedYearRange ??
-                                              provider.defaultYear,
-                                        )));
-                              },
-                              child: IndexCard(
-                                count: provider.count,
-                                icon: Icons.school,
-                                child: 'All Students',
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) => Announcement(
-                                            year: provider.selectedYearRange ??
-                                                provider.defaultYear,
-                                          )),
-                                );
-                              },
-                              child: IndexCard(
-                                count: provider.announcement,
-                                icon: Icons.mail,
-                                child: 'Announcement',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+          child: Consumer<DashboardProvider>(
+            builder: (context, provider, child) {
+              return SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    _buildHeaderImage(),
+                    _buildYearDropdownAndRefreshButton(provider),
+                    _buildDashboardCards(context, provider),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeaderImage() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        constraints: const BoxConstraints(
+          maxWidth: 810,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset(
+            'assets/img/nmscst_bg.jpg',
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildYearDropdownAndRefreshButton(DashboardProvider provider) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        constraints: const BoxConstraints(
+          maxWidth: 810,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: DropdownButtonFormField<String>(
+                value: provider.selectedYearRange,
+                decoration: const InputDecoration(
+                  label: Text("School Year"),
+                  contentPadding: EdgeInsets.zero,
+                ),
+                items: provider.schoolYears.map((String yearRange) {
+                  return DropdownMenuItem<String>(
+                    value: yearRange,
+                    child: Text(yearRange),
                   );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  provider.setSelectedYearRange(newValue);
                 },
               ),
             ),
-          ),
+            ElevatedButton(
+              onPressed: provider.refresh,
+              child: const Text("Reload Data / Refresh"),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildDashboardCards(
+      BuildContext context, DashboardProvider provider) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        spacing: 8.0,
+        runSpacing: 8.0,
+        children: <Widget>[
+          _buildDashboardCard(
+            context,
+            provider,
+            'List of Late',
+            provider.late,
+            Icons.timelapse_sharp,
+            () => AllLateStudent(
+                year: provider.selectedYearRange ?? provider.defaultYear),
+          ),
+          _buildDashboardCard(
+            context,
+            provider,
+            'Outside Range',
+            provider.outside.toString(),
+            Icons.location_off_sharp,
+            () => AllOutsideRange(ids: provider.outsideIds),
+          ),
+          _buildDashboardCard(
+            context,
+            provider,
+            'Absent',
+            provider.absent,
+            Icons.person_off_outlined,
+            () => AllAbsentStudent(
+                year: provider.selectedYearRange ?? provider.defaultYear),
+          ),
+          _buildDashboardCard(
+            context,
+            provider,
+            'All Establishment',
+            provider.count_estab,
+            Icons.location_city,
+            () => const AllEstablishment(),
+          ),
+          _buildDashboardCard(
+            context,
+            provider,
+            'All Students',
+            provider.count,
+            Icons.school,
+            () => CoursesPage(
+                year: provider.selectedYearRange ?? provider.defaultYear),
+          ),
+          _buildDashboardCard(
+            context,
+            provider,
+            'Announcement',
+            provider.announcement,
+            Icons.mail,
+            () => Announcement(
+                year: provider.selectedYearRange ?? provider.defaultYear),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDashboardCard(
+    BuildContext context,
+    DashboardProvider provider,
+    String title,
+    dynamic count,
+    IconData icon,
+    Widget Function() pageBuilder,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => pageBuilder()),
+        );
+      },
+      child: IndexCard(
+        count: count,
+        icon: icon,
+        child: title,
       ),
     );
   }
